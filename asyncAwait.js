27346c306704fe92249657e8523a526c4431cedd
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-05-02 20:33:43 -0400

    fix async await

--- asyncAwait.js
+++ asyncAwait.js
@@ -8,30 +8,29 @@ class HttpError extends Error {
 
 
 async function loadJson(url) {
-    await fetch(url)
-    .then(response => {
+    const response = await fetch(url);
     if (response.status == 200) {
     return response.json();
     } else {
     throw new HttpError(response);
     }
-});
 }
     
 async function demoGithubUser() {
     let name = prompt("Enter a name?", "vanimar");
-    return await loadJson(`https://api.github.com/users/${name}`).then(user => {
-    alert(`Full name: ${user.name}.`);
-    return user;
-    })
-    .catch(err => {
-    if (err instanceof HttpError && err.response.status == 404) {
-    alert("No such user, please reenter.");
-    return demoGithubUser();
-    } else {
-    throw err;
-    }
-    });
+
+    try{
+        const user=  await loadJson(`https://api.github.com/users/${name}`);
+            alert(`Full name: ${user.name}.`);
+            return user;
+    } catch(err){
+        if (err instanceof HttpError && err.response.status == 404) {
+            alert("No such user, please reenter.");
+            return demoGithubUser();
+            } else {
+            throw err;
+            }
+        }
     }
 
 demoGithubUser();
