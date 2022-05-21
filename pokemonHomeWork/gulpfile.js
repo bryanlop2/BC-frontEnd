6dac62cd676cad749e1548420d98e0d5aaba06bb
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-05-20 20:42:00 -0400

    added the clean script in the gulpfile

--- pokemonHomeWork/gulpfile.js
+++ pokemonHomeWork/gulpfile.js
@@ -2,7 +2,9 @@ var    gulp = require('gulp'),
 concat = require('gulp-concat'),
 uglify = require('gulp-uglify'),
 minifycss =require('gulp-minify-css'),
-concatify = require('gulp-concat');
+concatify = require('gulp-concat'),
+less = require('gulp-less'),
+clean = require('gulp-clean');
 
 
 gulp.task('minify-js', function () {
@@ -20,8 +22,20 @@ gulp.task('minify-css', function () {
 });
 
 gulp.task('concatify', function() {
-    return gulp.src('src/pokemon.js')
-    .pipe(uglify())
-    .pipe(concatify('all.js'))
-    .pipe(gulp.dest('dist/js'));
+  return gulp.src('src/pokemon.js')
+  .pipe(uglify())
+  .pipe(concatify('all.js'))
+  .pipe(gulp.dest('dist/js'));
 });
+
+gulp.task('less', function() {
+  return gulp.src('src/pokemon.less')
+  .pipe(less())
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('clean', function(){
+  return gulp.src('css/')
+  .pipe(clean({force: true}))
+  .pipe(gulp.dest('dist'));
+})
