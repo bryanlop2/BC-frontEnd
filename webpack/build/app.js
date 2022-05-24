f9f95e2eefce0d97537273539669839002a35f0c
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-05-24 18:05:57 -0400

    after classes webpack example running

--- webpack/build/app.js
+++ webpack/build/app.js
@@ -1,5 +1,14 @@
 /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
-console.log('Hello world');
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
 /******/ })()
 ;
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
