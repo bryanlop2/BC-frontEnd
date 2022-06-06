36240bc0696b1b1901fed7262704c7b0b37eba64
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-06-06 15:12:25 -0400

    Updated pokemon homework with the rigth console logs

--- decorators/pokemon.ts
+++ decorators/pokemon.ts
@@ -1,31 +1,41 @@
 type Move = {
-    name: string,
-    power: number
+  name: string,
+  power: number
 };
 
-function checkPP() {
-  return function(move: Move){
-    
+function checkPP(){
+  return function (target: Object,
+  propertyKey: string,
+  descriptor: PropertyDescriptor){
+    const newvalue = descriptor.value;
+    descriptor.value = function() {
+      if(this.ppAvailable < 1){
+        console.log(`Cannot use this move ${propertyKey} not enough PP`)
+      }else{
+        newvalue.apply(this, arguments)
+        console.log(`Used move: ` + propertyKey)
+      }
+    }
   }
 }
 
 class Pokemon {
-  name: string;
-  ppAvailable = 1;
-  constructor(name: string, ppAvailable: number) {
-    this.name = name;
-    this.ppAvailable = ppAvailable;
-  }
-  
-  @checkPP()
-  figth(move: Move) {
-    console.log(`${this.name} used ${move?.name}!`);
-    this.ppAvailable -= 1;
-  }
+name: string;
+ppAvailable = 1;
+constructor(name: string, ppAvailable: number) {
+  this.name = name;
+  this.ppAvailable = ppAvailable;
+}
 
-  calculateDamage(move: any) {
-    return move.power;
-  }
+@checkPP()
+figth(move: Move) {
+  console.log(`${this.name} used ${move?.name}!`);
+  this.ppAvailable -= 1;
+}
+
+calculateDamage(move: any) {
+  return move.power;
+}
 }
 
 const thunderbolt: Move = {name: 'thunderbolt', power: 90};
