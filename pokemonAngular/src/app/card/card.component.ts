e0de59bb45fd0716f2de7d52b2041495522313ad
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-06-19 20:24:47 -0400

    getting the pokemon names without any style

--- pokemonAngular/src/app/card/card.component.ts
+++ pokemonAngular/src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { PokedexService } from '../services/pokedex.service';
 
 
 @Component({
@@ -7,9 +8,48 @@ import { Component } from "@angular/core";
     styleUrls: ['./card.component.css']
 })
 
-export class CardComponent {
-    private pokemonURL = 'https://pokeapi.co/api/v2/';
+export class CardComponent implements OnInit {
+    pokemons = [];
+    data: any[] = [];
 
-    constructor() { }
+    constructor(private pokedexService: PokedexService) { }
 
+    ngOnInit(): void {
+        this.getPokemons();
+    }
+
+    getPokemons(): void {
+        let pokemonResults;
+    
+        for (let i = 1; i <= 100; i++) {
+        this.pokedexService.getPokemons(i).subscribe(
+            res => {
+                pokemonResults = {
+                index: i,
+                name: res,
+                image: res,
+                color: res
+            };
+            //ponemos la data que viene del servicio en un arreglo
+            this.data.push(pokemonResults);
+            }
+        );
+        }
+    }
+
+
+    /*getData() {
+        this.pokedexService.getPokemons().subscribe((
+            pokemon: PokemonData[]
+        )=>{this.getPokemon(pokemon)})
+    }
+
+    getPokemon(pokemons: PokemonData[]) {
+        const result: Observable<PokemonDetails>[] = [];
+        pokemons.map((data: PokemonData) => {
+            result.push(
+                this.pokedexService.getPokemonDetail(data.name)
+            );
+        });
+    }*/
 }
--- pokemonAngular/src/app/services/pokedex.service.ts
+++ pokemonAngular/src/app/services/pokedex.service.ts
@@ -3,32 +3,25 @@ import { Injectable } from "@angular/core";
 import { map } from "rxjs";
 import { Observable } from "rxjs/internal/Observable";
 
-interface PokemonData {
+export interface PokemonData {
     name: string;
     url: string;
 }
 
-interface PokemonDetails {
+export interface PokemonDetails {
     id: number;
-    order: number;
-    name: string;
+    name: number;
+    sprite: string;
 }
 
 @Injectable()
 export class PokedexService {
-    private pokemonURL: string = 'https://pokeapi.co/api/v2/pokemon/';
-
+    private pokemonURL: string = 'https://pokeapi.co/api/v2/pokemon';
+    
     constructor(private http: HttpClient) { }
 
-    getPokemons(limit: number = 100) : Observable<PokemonData[]> {
-        return this.http.get<PokemonData[]>(this.pokemonURL + '&limit=' + limit)
-        .pipe(
-            map((pokemon: any) => pokemon.results)
-        )
-    }
-
-    getPokemonDetail(pokemon: number | string): Observable<PokemonDetails> {
-        return this.http.get<PokemonDetails>(this.pokemonURL + 'pokemon/' + pokemon);
+    getPokemons(pokemonId: string | number) {
+        return this.http.get(`${this.pokemonURL}/${pokemonId}`)
     }
 
 }
