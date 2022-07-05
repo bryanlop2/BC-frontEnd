03b0b102ef1775220b6249710d7e0c2d91606788
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-07-05 14:14:21 -0400

    fixed load mor button, now only display 50 pokemons

--- pokemonAngular/src/app/pokemons/pokemon-list/pokemon.component.ts
+++ pokemonAngular/src/app/pokemons/pokemon-list/pokemon.component.ts
@@ -1,85 +1,89 @@
-import { Component, OnInit, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  Input,
+} from '@angular/core';
 import { Router } from '@angular/router';
-import { PokedexService } from "../pokedex.service";
-import { Pokemon } from "../utils/types";
+import { PokedexService } from '../pokedex.service';
+import { Pokemon } from '../utils/types';
 import { colors, getPokemonImageUri } from '../utils/values';
 
-
 @Component({
-    selector: 'pokemon-card',
-    templateUrl: './pokemon.compoment.html',
-    styleUrls: ['./pokemon.component.css']
+  selector: 'pokemon-card',
+  templateUrl: './pokemon.compoment.html',
+  styleUrls: ['./pokemon.component.css'],
 })
-
 export class PokemonListComponent implements OnInit {
-    pokemonData: Pokemon[] = [];
-    findPokemon = '';
-    limit: number = 50;
-    offset: number = 0;
-    searchedPokemons: Pokemon[] = [];
-    loading: boolean;
+  pokemonData: Pokemon[] = [];
+  findPokemon = '';
+  limit: number = 50;
+  offset: number = 0;
+  searchedPokemons: Pokemon[] = [];
+  loading: boolean;
 
-    constructor(private pokedexService: PokedexService,
-        private router: Router) { 
-            this.loading = false;
-        }
-    
-    ngOnInit(): void {
-        this.getPage(this.offset);
-    }
+  constructor(private pokedexService: PokedexService, private router: Router) {
+    this.loading = false;
+  }
 
-    getPage(offset: number) {
-        this.pokedexService.getPokemonList(this.limit, offset)
-        .subscribe((data: {results: Pokemon[]}) => {this.pokemonData = [...this.pokemonData, ...data.results];
-        this.searchedPokemons = this.pokemonData});
-    }
+  ngOnInit(): void {
+    this.getPage();
+  }
 
-    getBackgroundColors(pokemon: Pokemon) {
-        let color
-        const mainColors = Object.values(colors);
-        if(pokemon.url)
-        color = mainColors[this.getId(pokemon.url)-1];
-        return color;
-    }
-
-    searchPokemon(pokemon: string) {
-        console.log(pokemon);
-        this.pokemonData = this.searchedPokemons.filter(
-            res => res.name.includes(pokemon)
-        )
-    }
-
-    getNumbers(pokemon: Pokemon) {
-        if(pokemon.url)
-        return ('00' + this.getId(pokemon.url)).slice(-3);
-        else
-        return undefined
-    }
-
-    getImages(pokemon: Pokemon) {
-        if(pokemon.url)
-        return getPokemonImageUri(this.getId(pokemon.url))
-        else
-        return null
-    }
-
-    getId(link: string) {
-        const url = link.split('/'),
-        index = url[url.length - 2];
-        return +index;
-    }
-
-    loadMorePokemons(): void {
+  getPage() {
+    this.pokemonData = [];
+    this.pokedexService
+      .getPokemonList(this.limit, this.offset)
+      .subscribe((data: { results: Pokemon[] }) => {
+        this.pokemonData = [...this.pokemonData, ...data.results];
+        this.searchedPokemons = this.pokemonData;
         this.offset += this.limit;
-        this.loading = true
-        setTimeout (() => {
-            this.loading = false;
-         }, 1500);
-        this.getPage(this.offset);
-    }
+      });
+  }
 
-    goToPokemonProfile(pokemon: Pokemon) {
-        if(pokemon.url)
-        this.router.navigate([`/pokedex/${this.getId(pokemon.url)}`])
-    }
+  getBackgroundColors(pokemon: Pokemon) {
+    let color;
+    const mainColors = Object.values(colors);
+    if (pokemon.url) color = mainColors[this.getId(pokemon.url) - 1];
+    return color;
+  }
+
+  searchPokemon(pokemon: string) {
+    this.pokemonData = this.searchedPokemons.filter((res) =>
+      res.name.includes(pokemon)
+    );
+  }
+
+  getNumbers(pokemon: Pokemon) {
+    if (pokemon.url) return ('00' + this.getId(pokemon.url)).slice(-3);
+    else return undefined;
+  }
+
+  getImages(pokemon: Pokemon) {
+    if (pokemon.url) return getPokemonImageUri(this.getId(pokemon.url));
+    else return null;
+  }
+
+  getId(link: string) {
+    const url = link.split('/'),
+      index = url[url.length - 2];
+    return +index;
+  }
+
+  loadMorePokemons(): void {
+    this.loading = true;
+    this.getPage();
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+    
+  }
+
+  goToPokemonProfile(pokemon: Pokemon) {
+    if (pokemon.url)
+      this.router.navigate([`/pokedex/${this.getId(pokemon.url)}`]);
+  }
 }
