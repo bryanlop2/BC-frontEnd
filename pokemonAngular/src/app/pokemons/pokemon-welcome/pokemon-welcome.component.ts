42d5c126e184893208dfb00ffa3ec0afd2703f84
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-07-15 21:57:34 -0400

    identation fix

---
+++ pokemonAngular/src/app/pokemons/pokemon-welcome/pokemon-welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+Component({
+  selector: '´pokemon-welcome',
+  templateUrl: './pokemon-welcome.component.html',
+  styleUrls: ['./pokemon-welcome.component.css'],
+});
+
+export class PokemonWelcomeComponent  {
+  constructor() {}
+}
