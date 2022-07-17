948b5683854b72f8510315325c989e1a12e174a9
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-07-17 16:59:29 -0400

    clean assets and imporve styles

--- pokemonAngular/src/app/pokemons/pokemon-welcome/pokemon-welcome.component.ts
+++ pokemonAngular/src/app/pokemons/pokemon-welcome/pokemon-welcome.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '´pokemon-welcome',
   templateUrl: './pokemon-welcome.component.html',
-  styleUrls: ['./pokemon-welcome.component.css'],
+  styleUrls: ['./pokemon-welcome.component.scss'],
 })
 
 export class PokemonWelcomeComponent implements OnInit {
